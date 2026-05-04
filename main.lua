002c47be59a47132c8526a606e206e423c4eac70
bluescreeen <Olivierus303@gmail.com>
2026-05-04 22:33:07 +0200

    added more feature to editor

---
+++ main.lua
@@ -0,0 +1,103 @@
+local pckgManager = {}
+
+function pckgManager.init_enums()
+    pckgManager.debugLevels = { debug=0, warnings=1, errors=2, none=3 } -- enum for debugging level (debugLevel)
+    pckgManager.printLevel = { message=0, warning=1, error=2} -- print level for pckgManager.print function
+    
+end
+
+
+function pckgManager.init_config() -- initial default config
+
+    -- Database
+    pckgManager.dbPath    = "/etc/pckg/db.json" -- path to db
+    pckgManager.dbContent = {} -- content in db
+
+    -- Install paths
+    pckgManager.installPaths = {
+        application="/bin/", -- application
+        service="/services/", -- service
+        test="/tests/", -- startup test
+        library="/libs/" -- library
+    }
+
+
+    -- dev stuff
+    pckgManager.debugLevel = pckgManager.debugLevels.debug -- debug level
+
+end
+
+
+local function prettyPrint(level, text ) -- pretty print msgs
+    local levelData = {
+        [0]={ icon="*", color="8" },
+        [1]={ icon="?", color="1" },
+        [2]={ icon="!", color="e" },
+    }
+
+    local Pre = " "..levelData[level].icon.." "
+    local PreColor = string.rep(levelData[level].color, #Pre)
+    local msg = " " .. text
+    local fullMsg = Pre .. msg
+
+    term.blit( -- print
+        fullMsg,  -- message
+        string.rep("0", #fullMsg), -- foreground ( full white )
+        PreColor .. string.rep("7", #msg) -- background ( msg color + gray )
+    )
+
+    print() -- new line
+end
+
+
+function pckgManager.print(level, text ) -- print
+    if level>=pckgManager.debugLevel then -- check if level of message is lower or eq to debug level
+        prettyPrint(level, text)
+    end
+end
+
+
+function pckgManager.init() -- init (load db etc etc)
+    pckgManager.print(pckgManager.printLevel.message, "pckgManager.init() - start" )
+
+    if fs.exists(pckgManager.dbPath) then -- if db exists load it
+        pckgManager.print(pckgManager.printLevel.message, "db file found." )
+    
+        local DB_FILE = fs.open(pckgManager.dbPath, 'r') -- open db file
+
+        pckgManager.dbContent = textutils.unserializeJSON(DB_FILE.readAll()) -- load db content
+        DB_FILE.close() -- close file
+    else -- if no db file found
+        pckgManager.print(pckgManager.printLevel.warning, "no db file found" )
+    end
+
+    pckgManager.print(pckgManager.printLevel.message, "pckgManager.init() - end" )
+end
+
+
+function pckgManager.install(package, version) -- install package
+    
+end
+
+
+function pckgManager.remove(package) -- remove packages
+    
+end
+
+
+function pckgManager.list() -- list all installed packages
+    
+end
+
+
+function pckgManager.search(package) -- search for package in db
+    
+end
+
+
+function pckgManager.sync() -- sync db from sources
+    
+end
+
+
+return pckgManager
