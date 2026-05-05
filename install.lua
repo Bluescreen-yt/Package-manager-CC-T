1f67842c8b22e9c039d7fbe6772615e4c37cf4d0
bluescreeen <Olivierus303@gmail.com>
2026-05-05 13:24:11 +0200

    umm install.lua no longer needs to get pckg from github raw thingy if pckg is already installed. it just uses the pckg installed to update it

--- install.lua
+++ install.lua
@@ -1,6 +1,14 @@
 -- wget run https://raw.githubusercontent.com/Bluescreen-yt/Package-manager-CC-T/refs/heads/main/install.lua
 
-local pckgCode = http.get("https://raw.githubusercontent.com/Bluescreen-yt/Package-manager-CC-T/refs/heads/main/main.lua")
+if fs.exists("/bin/pckg") then
+    print("pckg is already installed! updating..")
+    local pckgCodeFile = fs.open("/bin/pckg", "r")
+    local pckgCode = pckgCodeFile.readAll()
+    pckgCodeFile.close()
+else
+    local pckgCode = http.get("https://raw.githubusercontent.com/Bluescreen-yt/Package-manager-CC-T/refs/heads/main/main.lua")
+end
+
 
 local pckg = load(pckgCode.readAll())
 pckgCode.close()
