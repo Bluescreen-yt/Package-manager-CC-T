d94749c8535fdad3f7c2b1f203851c6a85c3f0d5
bluescreeen <Olivierus303@gmail.com>
2026-05-07 18:40:50 +0200

    added createStartupFile command to create startupfile to load aliases

--- run.lua
+++ run.lua
@@ -1,11 +1,29 @@
 local pckg = require("main")
 pckg.fullinit()
-pckg.debugLevel = pckg.debugLevels.none
+pckg.debugLevel = pckg.debugLevels.debug
 
 local commands = {
     update=nil,
+    createStartupFile=function ()
+        if fs.exists("/startup.lua") then
+            print("startup.lua already exists! are you sure you want to overite it? (y/n)")
+            local response = os.pullEvent("key")
+            if not keys.getName(response[2]) == "y" then
+                print("if you want to load aliases, run 'pckg loadAliases'")
+                return
+            end
+            
+        end
+
+        local startupFile = fs.open("/startup.lua", "w")
+        startupFile.write('shell.run("/bin/pckg/run.lua", "loadAliases)')
+        startupFile.close()
+        print("done! pckg will now load aliases on startup! :p")
+    end,
     install=nil,
     remove=nil,
+    loadAliases=pckg.loadAliases,
+    help=nil,
     list=pckg.list,
     listAll=function ()
         local Packages = pckg.listAll()
@@ -16,5 +34,9 @@ local commands = {
     end,
     sync=pckg.sync,
 }
+
+if not arg[1] then
+    commands.help()
+end
 
 commands[arg[1]](table.unpack(arg, 2))
