f706a15e5a9705a5c7d3fbdc34a6ffb77dfd364d
bluescreeen <Olivierus303@gmail.com>
2026-05-10 18:57:10 +0200

    fix.

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
--- wget run https://raw.githubusercontent.com/Bluescreen-yt/Package-manager-CC-T/refs/heads/dev/install.lua
+-- wget run https://raw.githubusercontent.com/Bluescreen-yt/Package-manager-CC-T/refs/heads/main/install.lua
 
 local pckgCode
-local Branch = "dev"
+local Branch = "main"
 
 if fs.exists("/bin/pckg") then
     print("pckg is already installed! updating..")
