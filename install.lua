671bf6f74dd615f837cf74abc0a793d4bd94e432
bluescreeen <Olivierus303@gmail.com>
2026-05-10 19:32:37 +0200

    tweaks for exp

--- install.lua
+++ install.lua
@@ -1,11 +1,11 @@
--- wget run https://raw.githubusercontent.com/Bluescreen-yt/Package-manager-CC-T/refs/heads/dev/install.lua
+-- wget run https://raw.githubusercontent.com/Bluescreen-yt/Package-manager-CC-T/refs/heads/experimental/install.lua
 
 local pckgCode
-local Branch = "dev"
+local Branch = "experimental"
 
 if fs.exists("/bin/pckg") then
     print("pckg is already installed! updating..")
-    pckgCode = fs.open("/bin/pckg/dev.lua", "r")
+    pckgCode = fs.open("/bin/pckg/main.lua", "r")
 
     if not pckgCode then
         print("failed to read existing pckg code! updating with online version..")
